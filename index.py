ace335907f532dfa4c16a0a1ce7d91e24d35a1fa
Gavin Wiener <gavinwiener@gmail.com>
2016-06-07 20:37:01 +0200

    Returns basic json

--- index.py
+++ index.py
@@ -1,9 +1,15 @@
 from flask import Flask, render_template
+from flask import jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def main():
     return render_template('index.html')
+	
+@app.route('/test-api')
+def api():
+    return jsonify(msg="This is a json msg")
 
 if __name__ == "__main__":
-    app.run()
+    #app.run()
+	app.run(debug=True) #reloads
